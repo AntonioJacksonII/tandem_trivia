3e9460c78f74cabf4ed30cb1225a0055090f6fcd
Antonio Jackson <antoniojackson@gmail.com>
2020-10-30 23:30:38 -0600

    Updaing answer model and spec to validate and test for boolean values

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Answer, type: :model do
     describe 'validations' do
         it { should validate_presence_of(:response) }
-        it { should validate_presence_of(:correct?) }
+        it { should validate_inclusion_of(:correct?).in_array([true, false]) }
     end
 
     describe 'relationships' do
