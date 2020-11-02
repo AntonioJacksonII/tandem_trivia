627857b6f24dd2323e6a240e82dd4e94e9c8728c
Antonio Jackson <antoniojackson@gmail.com>
2020-11-01 17:22:28 -0700

    Adding tests for root page welcome message and "Take Quiz" button

--- spec/features/root_spec.rb
+++ spec/features/root_spec.rb
@@ -2,9 +2,18 @@ require 'rails_helper'
 
 describe 'root page', type: :feature do
     it 'displays a welcome message' do
-        visit '/'
-        
+        visit root_path   
         expect(page).to have_content('Welcome to Tandem Trivia!')
-        expect(page).to have_button('Take Quiz')
     end
+
+    it 'displays a "Take Quiz" button that redirects to the questions index' do
+        question = Question.create!(ask: 'Is this a test question?')
+
+        visit root_path
+        expect(page).to have_button('Take Quiz')
+        
+        click_on 'Take Quiz'
+        expect(current_path).to eq(question_path(question.id))
+    end
+    
 end
