7a931d6a2ba767e2842d6e1b92862f5bb1fb57b5
Antonio Jackson <antoniojackson@gmail.com>
2020-11-01 17:23:12 -0700

    Adding questions index and show routes and results index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
 
-  resources :questions, only: [:index]
+  resources :questions, only: [:index, :show]
+
+  resources :results, only: [:index]
 end
